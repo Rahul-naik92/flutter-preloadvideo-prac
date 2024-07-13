7a2c25ea2c45ecd47fd4ab1d0c27041764f09cf4
rahulnaik92 <teamrahul.naik@gmail.com>
2024-07-13 20:22:34 +0530

    updated code

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+import 'dart:async';
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
-import 'package:learn_riverpod/page.dart';
-import 'package:preload_page_view/preload_page_view.dart';
+import 'package:flutter_cache_manager/flutter_cache_manager.dart';
+
 import 'package:video_player/video_player.dart';
 
 void main() {
@@ -12,81 +15,129 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'PreloadPageView Demo',
-      home: VideoPlayerPage(videoUrls: ["urls1"]),
+      title: 'Wikolo',
+      home: PlayerController(),
+
+      //  VideoReelsWidget(videoUrls: [
+      //   "https://livepin-admin-bucket.s3.ap-south-1.amazonaws.com/4_461f6b13fd.mp4",
+      //   "https://livepin-admin-bucket.s3.ap-south-1.amazonaws.com/1_9afae56794.mp4",
+      //   "https://livepin-admin-bucket.s3.ap-south-1.amazonaws.com/2_48617b2f56.mp4",
+      //   "https://livepin-admin-bucket.s3.ap-south-1.amazonaws.com/3_e119eeccbd.mp4",
+      //   "https://livepin-admin-bucket.s3.ap-south-1.amazonaws.com/5_e16b9681d8.mp4",
+      //   "https://livepin-admin-bucket.s3.ap-south-1.amazonaws.com/6_a3b5dc8442.mp4",
+      //   "https://livepin-admin-bucket.s3.ap-south-1.amazonaws.com/7_d3571f5ad7.mp4",
+      //   "https://livepin-admin-bucket.s3.ap-south-1.amazonaws.com/8_a4d0e1df61.mp4",
+      //   "https://livepin-admin-bucket.s3.ap-south-1.amazonaws.com/9_d92e917519.mp4",
+      //   "https://livepin-admin-bucket.s3.ap-south-1.amazonaws.com/10_41581ba49f.mp4",
+      //   "https://livepin-admin-bucket.s3.ap-south-1.amazonaws.com/11_a08cdfd856.mp4",
+      // ]),
     );
   }
 }
 
-class VideoPlayerPage extends StatefulWidget {
-  final List<String> videoUrls;
-
-  const VideoPlayerPage({Key? key, required this.videoUrls}) : super(key: key);
-
+class PlayerController extends StatefulWidget {
+  const PlayerController({Key? key}) : super(key: key);
   @override
-  _VideoPlayerPageState createState() => _VideoPlayerPageState();
+  State<PlayerController> createState() => _PlayerControllerState();
 }
 
-class _VideoPlayerPageState extends State<VideoPlayerPage> {
-  late PreloadPageController _pageController;
-  late List<VideoPlayerController> _videoControllers;
+class _PlayerControllerState extends State<PlayerController> {
+  //properties
+
+  //to check which index is currently played
+  int currentIndex = 0;
+
+  //static content
+  final List<String> urls = const [
+    "https://livepin-admin-bucket.s3.ap-south-1.amazonaws.com/4_461f6b13fd.mp4",
+    "https://livepin-admin-bucket.s3.ap-south-1.amazonaws.com/1_9afae56794.mp4",
+    "https://livepin-admin-bucket.s3.ap-south-1.amazonaws.com/2_48617b2f56.mp4",
+    "https://livepin-admin-bucket.s3.ap-south-1.amazonaws.com/3_e119eeccbd.mp4",
+    "https://livepin-admin-bucket.s3.ap-south-1.amazonaws.com/5_e16b9681d8.mp4",
+    "https://livepin-admin-bucket.s3.ap-south-1.amazonaws.com/6_a3b5dc8442.mp4",
+    "https://livepin-admin-bucket.s3.ap-south-1.amazonaws.com/7_d3571f5ad7.mp4",
+    "https://livepin-admin-bucket.s3.ap-south-1.amazonaws.com/8_a4d0e1df61.mp4",
+    "https://livepin-admin-bucket.s3.ap-south-1.amazonaws.com/9_d92e917519.mp4",
+    "https://livepin-admin-bucket.s3.ap-south-1.amazonaws.com/10_41581ba49f.mp4",
+    "https://livepin-admin-bucket.s3.ap-south-1.amazonaws.com/11_a08cdfd856.mp4",
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: MediaQuery.of(context).size.height,
+      child: PageView.builder(
+        scrollDirection: Axis.vertical,
+        itemCount: urls.length,
+        onPageChanged: (index) {
+          setState(() {
+            currentIndex = index;
+          });
+        },
+        itemBuilder: (ctx, index) {
+          return FeedItem(url: urls[index]);
+        },
+      ),
+    );
+  }
+}
+
+class FeedItem extends StatefulWidget {
+  // Url to play video
+  final String url;
+  const FeedItem({Key? key, required this.url}) : super(key: key);
+
+  @override
+  State<FeedItem> createState() => _FeedItemState();
+}
+
+class _FeedItemState extends State<FeedItem> {
+  // Player controller
+  late VideoPlayerController _controller;
 
   @override
   void initState() {
     super.initState();
-    _pageController = PreloadPageController();
-    _videoControllers = widget.videoUrls
-        .map((url) => VideoPlayerController.network(url))
-        .toList();
-
-    _videoControllers.forEach((controller) {
-      controller.initialize().then((_) {
-        setState(() {});
-      });
-    });
+    // Initialize player
+    initializePlayer(widget.url);
   }
 
+  // Initialize Video Player
+  void initializePlayer(String url) async {
+    final fileInfo = await checkCacheFor(url);
+    if (fileInfo == null) {
+      _controller = VideoPlayerController.network(url);
+    } else {
+      _controller = VideoPlayerController.file(fileInfo.file);
+    }
+
+    await _controller.initialize();
+    if (mounted) {
+      setState(() {
+        _controller.play();
+      });
+    }
+  }
+
+  // Check for cache
+  Future<FileInfo?> checkCacheFor(String url) async {
+    final FileInfo? value = await DefaultCacheManager().getFileFromCache(url);
+    return value;
+  }
+
+  // Dispose controller
   @override
   void dispose() {
-    _pageController.dispose();
-    _videoControllers.forEach((controller) => controller.dispose());
+    _controller.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: PreloadPageView.builder(
-          scrollDirection: Axis.vertical,
-          controller: _pageController,
-          itemCount: widget.videoUrls.length,
-          preloadPagesCount: 2,
-          itemBuilder: (context, index) {
-            final controller = _videoControllers[index];
-            return Center(
-              child: controller.value.isInitialized
-                  ? SizedBox.expand(
-                      child: AspectRatio(
-                        aspectRatio: controller.value.aspectRatio,
-                        child: VideoPlayer(controller),
-                      ),
-                    )
-                  : CircularProgressIndicator(),
-            );
-          },
-          onPageChanged: (index) {
-            // Pause all other video controllers except the one that is currently in view
-            for (int i = 0; i < _videoControllers.length; i++) {
-              if (i == index) {
-                _videoControllers[i].play();
-              } else {
-                _videoControllers[i].pause();
-              }
-            }
-          },
-        ),
-      ),
+    return AspectRatio(
+      aspectRatio: _controller.value.aspectRatio,
+      child: VideoPlayer(_controller),
     );
   }
 }
